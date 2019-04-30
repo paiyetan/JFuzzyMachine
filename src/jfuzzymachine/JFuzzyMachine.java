9c430faa8235d20b49a41d1b254e3850be4819ef
Paul Aiyetan <paul.aiyetan@nih.gov>
2019-04-30 17:01:39 -0400

    simplified exhaustive search implementation...

--- src/jfuzzymachine/JFuzzyMachine.java
+++ src/jfuzzymachine/JFuzzyMachine.java
@@ -269,7 +269,7 @@ public class JFuzzyMachine {
     }
     */
     
-    
+    /*
     public void searchHelper2(int inputIndex,
                                 int[] inputCombns,
                                     LinkedList<Rule> ruleList){
@@ -310,6 +310,144 @@ public class JFuzzyMachine {
         }
        
         return results; 
+    }
+    */
+    
+    
+    private ESearch searchHelper5(int numberOfInputs, 
+                                 String outputGene, 
+                                    String[] otherGenes) {
+        ESearch results = new ESearch();
+        
+        return results;
+    }
+
+    private ESearch searchHelper4(int numberOfInputs, 
+                                 String outputGene, 
+                                    String[] otherGenes) {
+        ESearch results = new ESearch();
+        
+        return results;
+    }
+
+    private ESearch searchHelper3(int numberOfInputs, 
+                                 String outputGene, 
+                                    String[] otherGenes) {
+        ESearch results = new ESearch();
+        
+        return results;
+    }
+
+    private ESearch searchHelper2(int numberOfInputs, 
+                                 String outputGene, 
+                                    String[] otherGenes) {
+        ESearch results = new ESearch();
+        // get all possible combinations of inputs (from otherGenes)
+        Combinations inputCombinations = new Combinations(otherGenes.length, numberOfInputs);
+        
+        double[] outputGeneExpValues = exprs.getRow(exprs.getRowIndex(outputGene), Table.TableType.DOUBLE);
+        Mean mean = new Mean();
+        double xBar = mean.evaluate(outputGeneExpValues); // average exoression value for output gene
+        double deviationSquaredSum = 0;
+        for(int i = 0; i < outputGeneExpValues.length; i++){
+            deviationSquaredSum = deviationSquaredSum + Math.pow((outputGeneExpValues[i] - xBar), 2);
+        }
+        // for each combination of inputs...
+        for (int[] inputCombns : inputCombinations) {
+            
+            LinkedList<Rule> ruleCombns = new LinkedList();
+            for(int i = 1; i <= 3; i++){
+                for(int j = 1; j <= 3; j++){
+                    for(int k = 1; k <= 3; k++){
+
+                        for(int l = 1; l <= 3; l++){
+                            for(int m = 1; m <= 3; m++){
+                                for(int n = 1; n <= 3; n++){
+
+                                    ruleCombns.add(new Rule(i,j,k));
+                                    ruleCombns.add(new Rule(l,m,n));
+                                    
+                                    double[] xCaretValues = new double[outputGeneExpValues.length];
+                                    double residualSquaredSum = 0;
+                                    
+                                    for(int indx = 0; indx < xCaretValues.length; indx++){
+                                        //get input genes 
+                                        // NOTE: there are two inputs here
+                                        String input1 = otherGenes[inputCombns[0]];
+                                        String input2 = otherGenes[inputCombns[1]];
+                                        //get fuzzyValues of input genes, 
+                                        FuzzySet fz1 = fMat[exprs.getRowIndex(input1)][indx];
+                                        FuzzySet fz2 = fMat[exprs.getRowIndex(input2)][indx];
+                                        //get Zx,y,or z values using the Union Rule Configuration (URC) on rule combination
+                                        double zx = fz1.get(i) + fz2.get(l);
+                                        double zy = fz1.get(j) + fz2.get(m);
+                                        double zz = fz1.get(k) + fz2.get(n);
+                                        //get defuzzified (xCaretValue) value of Z @ position indx
+                                        double dfz = this.deFuzzify(new FuzzySet(zx, zy, zz));
+                                        xCaretValues[indx] = dfz;
+                                        // compute residual and residual squared sum...
+                                        residualSquaredSum = residualSquaredSum + Math.pow((outputGeneExpValues[indx] - dfz), 2);
+                                        
+                                    }
+                                    
+                                    //compute error..
+                                    double E = 1 - (residualSquaredSum/deviationSquaredSum);
+
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+            
+        }
+                    
+        
+        
+        
+        return results;
+    }
+
+    private ESearch searchHelper1(int numberOfInputs, 
+                                 String outputGene, 
+                                    String[] otherGenes) {
+        ESearch results = new ESearch();
+        
+        
+        return results;
+    }
+
+      
+    
+    
+    public ESearch searchHelper(int numberOfInputs, String outputGene, String[] otherGenes){
+        ESearch results = new ESearch();
+        
+        switch(numberOfInputs){
+            case 5:
+                results = searchHelper5(numberOfInputs, 
+                        outputGene, otherGenes);
+                break;
+            case 4:
+                results = searchHelper4(numberOfInputs, 
+                        outputGene, otherGenes);
+                break;
+            case 3:
+                results = searchHelper3(numberOfInputs, 
+                        outputGene, otherGenes);
+                break;
+            case 2:
+                results = searchHelper2(numberOfInputs, 
+                        outputGene, otherGenes);
+                break;
+            default:
+                results = searchHelper1(numberOfInputs, 
+                        outputGene, otherGenes);
+                break;
+                
+        }
+        return results;
     }
      
     public ESearch search() throws FileNotFoundException {
@@ -513,7 +651,7 @@ public class JFuzzyMachine {
         
     }
 
-      
+    
  
     class PermutateArray {        
         
